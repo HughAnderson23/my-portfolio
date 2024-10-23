9c01c4d2a7324437c18aeeaa1019f0593cccdf59
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-10-23 10:48:25 -0400

    Added three more projects

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import './App.css';
 import { Github, ExternalLink, Mail } from 'lucide-react';
 import burgerThumbnail from './thumbnails/burger.PNG';
 import multiplayerThumbnail from './thumbnails/multiplayer.PNG';
+import starwarsThumbnail from './thumbnails/starwarsblog.PNG';
+import f1raceThumbnail from './thumbnails/f1racestats.PNG';
+import textThumbnail from './thumbnails/3dtext.PNG';
 
 const Portfolio = () => {
   const projects = [
     {
       title: "Burger Builder Web App",
-      description: "An app where users can make and purchase hamburgers.",
+      description: "An app where users can make and purchase hamburgers. Make sure to sign up to create an account and then use those credentials to login.",
       githubLink: "https://github.com/HughAnderson23/Burger-Builder",
       liveLink: "https://my-first-fstack-c4007ac93293.herokuapp.com/home",
-      tags: ["JavaScript", "React.js", "Flask", "Python", "PostgreSQL", "sqlAlchemy", "Restful APIs", "Heroku"],
+      tags: ["JavaScript", "React.js", "Flask", "Python", "PostgreSQL", "sqlAlchemy", "Restful APIs", "Heroku", "Full Stack"],
       thumbnail: burgerThumbnail 
     },
     {
       title: "Multiplayer Web-Based Physics Game",
-      description: "Real-time 20 player web game with seamless state synchronization. Has physics based character movement and interactions.",
+      description: "Real-time 20 player web game with seamless state synchronization. Has physics based character movement and interactions. Once your character gets big enough, press space bar to split. Javascript was used for Frontend and Backend development.",
       githubLink: "https://github.com/HughAnderson23/my-pelican-game-master",
       liveLink: "https://my-pelican-game-master-e2eee1009e58.herokuapp.com/",
-      tags: ["Node.js", "THREE.js", "Express", "Socket.io", "WebGL", "Heroku"],
+      tags: ["Node.js", "THREE.js", "Express", "Socket.io", "WebGL", "Heroku", "Full Stack"],
       thumbnail: multiplayerThumbnail 
-    }
+    },
+    {
+      title: "Star Wars Blog",
+      description: "Web application that demonstrates CRUD operations using Flux architecture. SWAPI is used to fetch data. Users can save and view their favorite planets, characters and spaceships. As well as learn more about each of their selections.",
+      githubLink: "https://github.com/HughAnderson23/Star-Wars-Blog",
+      liveLink: "https://star-wars-blog-orpin.vercel.app/",
+      tags: ["Node.js", "React.js", "Flux", "Vercel", "Restful APIs", "Bootstrap", "Full Stack"],
+      thumbnail: starwarsThumbnail 
+    },
+    {
+      title: "F1 Racing Results",
+      description: "Web app I made for my favorite sport. Features real-time synchronization with F1 API. Users can view current and historical results for each race and driver, as well as the schedule for upcoming races.",
+      githubLink: "https://github.com/HughAnderson23/F1-Racing-Statistics",
+      liveLink: "https://f1-racing-statistics.vercel.app/qualies",
+      tags: ["Node.js", "Webpack", "React.js", "Vercel", "Restful APIs", "Full Stack"],
+      thumbnail: f1raceThumbnail 
+    },
+    {
+      title: "3D Interactive Text/Scene",
+      description: "A web scene I made to showcase the skills I learned useing THREE.js. The scene allows users to interact with 3D text, complete with custom textures in the form of matcaps. Custom matcaps are used to give the text a metallic look. There are no light sources in the scene even though the text has lighting, it allows the website to remain very performant.",
+      githubLink: "https://github.com/HughAnderson23/MyProfile",
+      liveLink: "https://interactive-text.vercel.app/",
+      tags: ["Node.js", "Vite","lil gui", "React.js", "Vercel", "Front-End"],
+      thumbnail: textThumbnail 
+    },
   ];
 
   return (
