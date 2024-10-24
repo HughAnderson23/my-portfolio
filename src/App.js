c773c192bd806f7ccc0786a0cb58fb33623b9948
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-10-24 11:13:36 -0400

    vercel analytics installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react";
 import { Github, ExternalLink, Mail } from 'lucide-react';
 import burgerThumbnail from './thumbnails/burger.PNG';
 import multiplayerThumbnail from './thumbnails/multiplayer.PNG';
@@ -52,6 +53,7 @@ const Portfolio = () => {
   ];
 
   return (
+    <>
     <div className="min-h-screen bg-gray-100">
       {/* Header/Nav */}
       <nav className="bg-white shadow-md">
@@ -112,6 +114,8 @@ const Portfolio = () => {
         </div>
       </div>
     </div>
+    <Analytics /> 
+    </> 
   );
 };
 
